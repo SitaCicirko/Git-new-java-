f6c3907b850cd475057555338e844f2177c817d6
SitaCicirko <108766560+SitaCicirko@users.noreply.github.com>
2023-03-20 22:45:12 +0000

    First commit

---
+++ src/test/java/com/example/kodilla/KodillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
